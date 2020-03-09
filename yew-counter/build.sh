34cf97048b8f2ddb4e477ce6811befc5ad208323
Hanabusa Masahiro <masahiro.hanabusa@brother.co.jp>
2020-03-09 12:58:25 +0900

    Yew 0.13 with web-sys no longer requires npm rollup

--- yew-counter/build.sh
+++ yew-counter/build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ ! -f ./node_modules/rollup/dist/bin/rollup ]; then
-  npm install rollup
-fi
-
-wasm-pack build --target web && \
-./node_modules/rollup/dist/bin/rollup -i main.js -f iife -o pkg/bundle.js
+wasm-pack build --target web
--- yew-counter/main.js
+++
@@ -1,6 +0,0 @@
-import init, { run_app } from './pkg/yew_counter.js';
-async function main() {
-   await init('/pkg/yew_counter_bg.wasm');
-   run_app();
-}
-main();
