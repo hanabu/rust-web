a3bbb689bf15b98c780e8ef67207b16d639fe88d
Hanabusa Masahiro <masa@hanabusa.net>
2020-03-10 01:05:53 +0900

    No TypeScript

--- yew-counter/build.sh
+++ yew-counter/build.sh
@@ -3,4 +3,4 @@
 # cargo build --release --target wasm32-unknown-unknown
 # wasm-bindgen --target no-modules --out-dir pkg --no-typescript target/wasm32-unknown-unknown/release/yew_counter.wasm
 
-wasm-pack build --target no-modules
+wasm-pack build --target no-modules --no-typescript
--- yew-helloworld/build.sh
+++ yew-helloworld/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wasm-pack build --target no-modules
+wasm-pack build --target no-modules --no-typescript
