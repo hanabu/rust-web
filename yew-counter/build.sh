ff2e688d539e73f2de912abbf4dd59f3eccd4c08
Hanabusa Masahiro <masa@hanabusa.net>
2020-03-09 20:02:20 +0900

    Add comment to build without wasm-pack

--- yew-counter/build.sh
+++ yew-counter/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# cargo build --release --target wasm32-unknown-unknown
+# wasm-bindgen --target no-modules --out-dir pkg --no-typescript target/wasm32-unknown-unknown/release/yew_counter.wasm
+
 wasm-pack build --target no-modules
