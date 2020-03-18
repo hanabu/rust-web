hanabu/rust-web

commit 72b1bc7a2ec988c3e4367d33d37aec84faf2dd71

Build without wasm-pack

yew-helloworld/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-wasm-pack build --target no-modules --no-typescript
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
+             target/wasm32-unknown-unknown/release/yew_hello.wasm
+
+# wasm-pack build --target no-modules --no-typescript

yew-counter/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# cargo build --release --target wasm32-unknown-unknown
-# wasm-bindgen --target no-modules --out-dir pkg --no-typescript target/wasm32-unknown-unknown/release/yew_counter.wasm
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
+             target/wasm32-unknown-unknown/release/yew_counter.wasm
 
-wasm-pack build --target no-modules --no-typescript
+# wasm-pack build --target no-modules --no-typescript

commit 211b99383bdedc3d2d45e8fa25cb86ce9318ce6b

Fix build script (regression in 72b1bc7a)

yew-helloworld/build.sh
@@ -2,6 +2,6 @@
 
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
-             target/wasm32-unknown-unknown/release/yew_hello.wasm
+             target/wasm32-unknown-unknown/release/yew_helloworld.wasm
 
 # wasm-pack build --target no-modules --no-typescript
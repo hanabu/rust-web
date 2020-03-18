211b99383bdedc3d2d45e8fa25cb86ce9318ce6b
Hanabusa Masahiro <masa@hanabusa.net>
2020-03-19 04:21:17 +0900

    Fix build script (regression in 72b1bc7a)

--- yew-helloworld/build.sh
+++ yew-helloworld/build.sh
@@ -2,6 +2,6 @@
 
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
-             target/wasm32-unknown-unknown/release/yew_hello.wasm
+             target/wasm32-unknown-unknown/release/yew_helloworld.wasm
 
 # wasm-pack build --target no-modules --no-typescript
