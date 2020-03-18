72b1bc7a2ec988c3e4367d33d37aec84faf2dd71
Hanabusa Masahiro <masa@hanabusa.net>
2020-03-18 23:18:25 +0900

    Build without wasm-pack

--- yew-counter/build.sh
+++ yew-counter/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# cargo build --release --target wasm32-unknown-unknown
-# wasm-bindgen --target no-modules --out-dir pkg --no-typescript target/wasm32-unknown-unknown/release/yew_counter.wasm
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
+             target/wasm32-unknown-unknown/release/yew_counter.wasm
 
-wasm-pack build --target no-modules --no-typescript
+# wasm-pack build --target no-modules --no-typescript
--- yew-helloworld/build.sh
+++ yew-helloworld/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-wasm-pack build --target no-modules --no-typescript
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
+             target/wasm32-unknown-unknown/release/yew_hello.wasm
+
+# wasm-pack build --target no-modules --no-typescript
