aed3ef216e3248c2cd3133e3ef9e920fde12be65
Hanabusa Masahiro <masahiro.hanabusa@brother.co.jp>
2020-03-19 22:03:11 +0900

    Split sample with modern Chrome, Firefox, Chromium Edge and Legacy IE-Edge

--- yew-helloworld/build.sh
+++ yew-helloworld/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --target no-modules --out-dir pkg --no-typescript \
+wasm-bindgen --target web --out-dir pkg --no-typescript \
+             target/wasm32-unknown-unknown/release/yew_helloworld.wasm
+
+wasm-bindgen --target no-modules --out-dir legacy --no-typescript \
              target/wasm32-unknown-unknown/release/yew_helloworld.wasm
 
 # wasm-pack build --target no-modules --no-typescript
